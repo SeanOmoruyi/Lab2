f6e49cce21da7b50eb0f7ad0410982b92f782c34
SeanOmoruyi <C22346841@mytudublin.ie>
2025-02-26 13:07:18 +0000

    Update NumberPrinter.java

--- NumberPrinter.java
+++ NumberPrinter.java
@@ -4,6 +4,12 @@ class NumberPrinter {
 
         int min = Math.min(a, b);
         int max = Math.max(a, b);
+
+        for (int i = min; i <= max; i++) {
+            System.out.println(i + "");
+        }
+
+        System.out.println();
     }
 
 }
